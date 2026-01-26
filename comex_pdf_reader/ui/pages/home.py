ce1412aa72437997759ca37916f5ddca83d6643b
ThiagoElux01 <thiago.farias@electrolux.com>
2026-01-26 09:08:29 -0300

    Update home.py

--- comex_pdf_reader/ui/pages/home.py
+++ comex_pdf_reader/ui/pages/home.py
@@ -3,13 +3,11 @@ import streamlit as st
 
 def render():
     st.subheader("Bem-vindo(a) 👋")
-    st.info("Este é o template inicial do COMEX PDF READER com login básico.")
+    st.info("Projeto piloto de extração de informações de pdfs, COMEX PDF READER")
     st.markdown(
         """
-        **Sugestões de próximos passos:**
         - Upload de PDFs
         - Extração de texto/tabelas
-        - Barra de progresso à direita
         - Exportação para Excel
         - Integração com Tasa SUNAT
         """
